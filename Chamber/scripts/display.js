a03366ce47e7f222511ab31b4a104603f98703f4
reiquel <carelary92@gmail.com>
2023-03-17 02:26:18 -0300

    afaf

--- Chamber/scripts/display.js
+++ Chamber/scripts/display.js
@@ -1,28 +1,53 @@
-const getRandomCompany = (businesses) => {
-    const randomIndex = Math.floor(Math.random() * businesses.length);
-    return businesses[randomIndex];
-  };
+// fetchData.js
+async function fetchCompanies() {
+    try {
+      const response = await fetch('data.json');
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const data = await response.json();
+      return data.businesses;
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
   
-  const displayCompanyInfo = (company) => {
-    const spotlights = ['spotlight1', 'spotlight2', 'spotlight3'];
+  function getRandomCompany(companies) {
+    return companies[Math.floor(Math.random() * companies.length)];
+  }
   
-    spotlights.forEach((spotlight) => {
-      const spotlightDiv = document.querySelector(`.${spotlight}`);
-      spotlightDiv.innerHTML = `
-        <h3>${company.name}</h3>
-        <p>${company.address}</p>
-        <p>${company.phone}</p>
-        <a href="${company.url}">Visit Website</a>
-        <img src="${company.imageurl}" alt="${company.name}" />
-      `;
-    });
-  };
+  async function displayCompanies() {
+    const companies = await fetchCompanies();
   
-  fetch('data.json')
-    .then((response) => response.json())
-    .then((data) => {
-      const randomCompany = getRandomCompany(data.businesses);
-      displayCompanyInfo(randomCompany);
-    })
-    .catch((error) => console.error('Error fetching data:', error));
+    const spotlight1 = getRandomCompany(companies);
+    let spotlight2;
+    do {
+      spotlight2 = getRandomCompany(companies);
+    } while (spotlight1.name === spotlight2.name);
+  
+    const spotlight3 = getRandomCompany(companies);
+  
+    document.querySelector('.spotlight1 b').textContent = spotlight1.name;
+    document.querySelector('.spotlight1 p').innerHTML = `
+      ${spotlight1.address}<br>
+      ${spotlight1.phone}<br>
+      <a href="${spotlight1.url}">${spotlight1.url}</a>
+    `;
+  
+    document.querySelector('.spotlight2 b').textContent = spotlight2.name;
+    document.querySelector('.spotlight2 p').innerHTML = `
+      ${spotlight2.address}<br>
+      ${spotlight2.phone}<br>
+      <a href="${spotlight2.url}">${spotlight2.url}</a>
+    `;
+  
+    document.querySelector('.spotlight3 b').textContent = spotlight3.name;
+    document.querySelector('.spotlight3 p').innerHTML = `
+      ${spotlight3.address}<br>
+      ${spotlight3.phone}<br>
+      <a href="${spotlight3.url}">${spotlight3.url}</a>
+    `;
+  }
+  
+  displayCompanies();
   
