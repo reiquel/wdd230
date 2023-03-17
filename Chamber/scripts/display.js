2a76323789fe76f7e5006eb7a6cf3953b9008b38
reiquel <carelary92@gmail.com>
2023-03-17 01:41:52 -0300

    fafa

--- Chamber/scripts/display.js
+++ Chamber/scripts/display.js
@@ -16,8 +16,6 @@ function getRandomBusiness(businesses) {
 
 function displayBusinessInfo(business) {
   const spotlight1 = document.querySelector(".spotlight1");
-  const spotlight2 = document.querySelector(".spotlight2");
-  const spotlight3 = document.querySelector(".spotlight3");
 
   const name = document.createElement("h3");
   name.textContent = business.name;
@@ -42,15 +40,63 @@ function displayBusinessInfo(business) {
   spotlight1.appendChild(url);
   spotlight1.appendChild(image);
 
-  spotlight2.appendChild(name);
-  spotlight2.appendChild(address);
-  spotlight2.appendChild(phone);
-  spotlight2.appendChild(url);
-  spotlight2.appendChild(image);
-
-  spotlight3.appendChild(name);
-  spotlight3.appendChild(address);
-  spotlight3.appendChild(phone);
-  spotlight3.appendChild(url);
-  spotlight3.appendChild(image);
 }
+
+
+function displayBusinessInfo(business) {
+    const spotlight2 = document.querySelector(".spotlight2");
+  
+    const name = document.createElement("h3");
+    name.textContent = business.name;
+  
+    const address = document.createElement("p");
+    address.textContent = business.address;
+  
+    const phone = document.createElement("p");
+    phone.textContent = business.phone;
+  
+    const url = document.createElement("a");
+    url.href = business.url;
+    url.textContent = "Visit Website";
+  
+    const image = document.createElement("img");
+    image.src = business.imageurl;
+    image.alt = `${business.name} logo`;
+  
+  
+    spotlight2.appendChild(name);
+    spotlight2.appendChild(address);
+    spotlight2.appendChild(phone);
+    spotlight2.appendChild(url);
+    spotlight2.appendChild(image);
+
+  }
+
+  
+function displayBusinessInfo(business) {
+    const spotlight3 = document.querySelector(".spotlight3");
+  
+    const name = document.createElement("h3");
+    name.textContent = business.name;
+  
+    const address = document.createElement("p");
+    address.textContent = business.address;
+  
+    const phone = document.createElement("p");
+    phone.textContent = business.phone;
+  
+    const url = document.createElement("a");
+    url.href = business.url;
+    url.textContent = "Visit Website";
+  
+    const image = document.createElement("img");
+    image.src = business.imageurl;
+    image.alt = `${business.name} logo`;
+
+
+    spotlight3.appendChild(name);
+    spotlight3.appendChild(address);
+    spotlight3.appendChild(phone);
+    spotlight3.appendChild(url);
+    spotlight3.appendChild(image);
+  }
