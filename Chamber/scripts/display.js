b79e105f62a3ac87447900cfb8c3ebe9a11f4ace
reiquel <carelary92@gmail.com>
2023-03-18 21:39:50 -0300

    afafa

--- Chamber/scripts/display.js
+++ Chamber/scripts/display.js
@@ -14,7 +14,7 @@ function getRandomInt(max) {
       <h3>${company.name}</h3>
       <p>${company.address}</p>
       <p>${company.phone}</p>
-      <p><a>${company.url}</a></p>
+      <a>${company.url}</a>
       <img src="${company.imageurl}" alt="${company.name}" />
     `;
   }
